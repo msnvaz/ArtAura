f52810693f9fcfa5e9110dec0c732bbe4c3bd27d
Pawani Kumari <pawanipushpakumari7@gmail.com>
2025-06-30 12:27:02 +0530

    login signup back end

--- client/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-import autoprefixer from 'autoprefixer';
-
-/** @type {import('postcss-load-config').Config} */
-export default {
-  plugins: [autoprefixer],
-};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,19 +11,23 @@ import UploadArtWork from './pages/Artist/UploadArtWork';
 import ArtworkDetail from './pages/Artist/ArtWorkDetail';
 import ArtistDashboard from './pages/Artist/ArtistDashBoard';
 import ArtistPortfolio from './pages/Artist/ArtistPortfolio';
+import Signup from './components/auth/signup2';
+import ShopRegisterPage from './components/auth/shopSignup';
+import CommunityPage from './pages/CommunityPage';
 
 // Components
-import Header from './components/Header';
-import Footer from './components/Footer';
+//import Header from './components/Header';
+import Footer from './components/common/Footer';
+
+// Contexts
 
 
 function App() {
   return (
     <Router>
-
-      <div className="App d-flex flex-column min-vh-100 w-100">
-        <Header />
-    <main className="flex-grow-1 w-100">
+      {/* <div className="App d-flex flex-column min-vh-100 w-100"> */}
+        {/* <Header /> */}
+    {/* <main className="flex-grow-1 w-100"> */}
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/marketplace" element={<Marketplace />} />
@@ -32,12 +36,15 @@ function App() {
           <Route path="/artworks/:id" element={<ArtworkDetail />} />
           <Route path="/artist/artistdashboard" element={<ArtistDashboard />} /> 
           <Route path="/artist/artistportfolio" element={<ArtistPortfolio />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/register/shop" element={<ShopRegisterPage />} />
+          <Route path="/community" element={<CommunityPage />} />
 
         </Routes>
-        </main>
+        {/* </main> */}
       <Footer />
 
-      </div>
+       {/* </div> */}
     </Router>
   );
 }
