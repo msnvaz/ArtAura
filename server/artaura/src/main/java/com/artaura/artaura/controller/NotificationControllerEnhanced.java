d8cd418ab0750cec062b9c7d4fb2823021337de4
Pawani Kumari <pawanipushpakumari7@gmail.com>
2025-10-19 16:24:10 +0530

    correct the notification issue

--- server/artaura/src/main/java/com/artaura/artaura/controller/NotificationControllerEnhanced.java
+++ server/artaura/src/main/java/com/artaura/artaura/controller/NotificationControllerEnhanced.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/notifications")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"}, allowCredentials = "true")
 public class NotificationControllerEnhanced {
 
     @Autowired
